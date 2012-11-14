4cccea6b8a2f435fd8d13f604e23a141f1c8598a
Hadwin Zheng <hadwinzhy@gmail.com>
2012-11-14 22:10:54 +0800

    add tabbar.el and config

--- customize/my-utils.el
+++ customize/my-utils.el
@@ -11,6 +11,60 @@
 (autoload 'js2-mode "js2" nil t)
 (add-to-list 'auto-mode-alist '("\\.js$" . js2-mode))
 
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;tabbar;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;;tabbar
+(require 'tabbar)
+(tabbar-mode)
+(global-set-key (kbd "<M-up>")    'tabbar-backward-group)  
+(global-set-key (kbd "<M-down>")  'tabbar-forward-group)  
+(global-set-key (kbd "<M-left>")  'tabbar-backward-tab)  
+(global-set-key (kbd "<M-right>") 'tabbar-forward-tab)  
+  
+(setq  
+ tabbar-scroll-left-help-function nil   ;don't show help information  
+ tabbar-scroll-right-help-function nil  
+ tabbar-help-on-tab-function nil  
+ tabbar-home-help-function nil  
+ tabbar-buffer-home-button (quote (("") "")) ;don't show tabbar button  
+ tabbar-scroll-left-button (quote (("") ""))  
+ tabbar-scroll-right-button (quote (("") "")))  
+  
+; tab 分组
+(defun my-tabbar-buffer-groups ()  
+  "Return the list of group names the current buffer belongs to.  
+Return a list of one element based on major mode."  
+  (list  
+  (cond  
+    ((or (get-buffer-process (current-buffer))  
+         ;; Check if the major mode derives from `comint-mode' or  
+         ;; `compilation-mode'.  
+         (tabbar-buffer-mode-derived-p  
+          major-mode '(comint-mode compilation-mode)))  
+     "Process"  
+     )  
+    ((string-equal "*" (substring (buffer-name) 0 1))  
+     "Emacs Buffer"  
+     )  
+    ((eq major-mode 'dired-mode)  
+     "Dired"  
+     )  
+    (t  
+     "User Buffer"  
+     ))))  
+  
+(setq tabbar-buffer-groups-function 'my-tabbar-buffer-groups)  
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+
+
+
+
+
+
 ;; ibuffer mode
 (require 'ibuffer)
 (global-set-key (kbd "C-x C-b") 'ibuffer)
@@ -19,8 +73,14 @@
 
 ;; ido-mode
 ;; use to auto show all choices when switching buffers or files
+(require 'ido)
 (setq ido-enable-flex-matching t)
 (setq ido-everywhere t)
+;;;;;;;;;;;;;;;;;;;;;;;;add by Hadwin start;;;;;;;;;;;;;;;;;;;;;;
+(setq ido-confirm-unique-completion t)  ;; confirm only once
+(setq ido-default-buffer-method 'samewindow)  
+(setq ido-use-filename-at-point t)
+;;;;;;;;;;;;;;;;;;;;;;;add by Hadwin end;;;;;;;;;;;;;;;;;;;;;;;;;
 (ido-mode 1)
 
 ;; Make buffer name unique
