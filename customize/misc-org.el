6cbd9d598b7a0f8d2e3381fd57b6cdb40c89edf7
joshuafufu <joshuafufu@gmail.com>
2012-12-09 22:29:11 +0800

    hook iimage mode in org-mode.

--- customize/misc-org.el
+++ customize/misc-org.el
@@ -8,4 +8,27 @@
 (setq org-support-shift-select t)
 (setq org-log-done t)
 
+;; iimage in org
+;;(add-to-list 'iimage-mode-image-regex-alist
+;;	     (cons (concat "\\[\\[file:\\(~?" iimage-mode-image-filename-regex
+;;			   "\\)\\]")  1))
+
+(defun org-toggle-iimage-in-org ()
+  "display images in your org file"
+  (interactive)
+  (if (face-underline-p 'org-link)
+      (set-face-underline-p 'org-link nil)
+    (set-face-underline-p 'org-link t))
+  (iimage-mode))
+
+(add-hook 'org-mode-hook
+	  (lambda ()
+	    ;; display images
+	    (local-set-key "\M-I" 'org-toggle-iimage-in-org)
+	    ;; fix tab
+	    (local-set-key "\C-y" 'yank)
+	    ;; yasnippet (allow yasnippet to do its thing in org files
+	    (org-set-local 'yas/trigger-key [tab])
+	    (define-key yas/keymap [tab] 'yas/next-field-group)))
+
 (provide 'misc-org)
