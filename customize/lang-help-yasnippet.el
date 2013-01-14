7ce7275cc35be856473200f8691b7a5a35e2e0b2
joshuafufu <joshuafufu@gmail.com>
2013-01-13 18:59:46 -0800

    add new yasnippet

--- customize/lang-help-yasnippet.el
+++ customize/lang-help-yasnippet.el
@@ -1,5 +1,8 @@
 (require 'yasnippet)
 (yas-global-mode 1)
 
+(setq yas-snippet-dirs (append '("~/.emacs.d/customize/snippets/") yas-snippet-dirs))
+
+(yas-reload-all)
 
 (provide 'lang-help-yasnippet)
